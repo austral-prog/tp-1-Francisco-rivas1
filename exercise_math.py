0d17033cab37d774510788f4f34641d5c8f7d1d0
Francisco-rivas1 <frivas2@mail.austral.edu.ar>
2025-03-10 11:53:46 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,15 @@
+Valor de i1: 3 
+Valor de i2: 5
+Valor de i3: 8
+16
+Python is awesome
+Valor de x: Naranja, Valor de y: Naranja, valor de z: Naranja
+1.6
+3
+Entero i3: 10
+variable f3: 9.5
+El valor de 13 más 3 es: 16
+
 def math():
     a = 57
     b = 7
