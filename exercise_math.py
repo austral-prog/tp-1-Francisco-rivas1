86d6775e7672cf5eda3a456691b2c38ea377568a
Francisco-rivas1 <frivas2@mail.austral.edu.ar>
2025-03-17 11:24:12 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,15 +1,23 @@
-Valor de i1: 3 
-Valor de i2: 5
-Valor de i3: 8
-16
-Python is awesome
-Valor de x: Naranja, Valor de y: Naranja, valor de z: Naranja
-1.6
-3
-Entero i3: 10
-variable f3: 9.5
-El valor de 13 más 3 es: 16
+#Valor de i1: 3
+#Valor de i2: 5
+#Valor de i3: 8
+#16
+#Python is awesome
+#Valor de x: Naranja, valor de y: Naranja, valor de z: Naranja
+#1.6
+#3
+#El valor de 13 + 3 es: 16
+#Entero i3: 9
+#Variable f3: 9.5
+#El valor de 13 más -0.5 es: 12.5
 
-def math():
-    a = 57
-    b = 7
+
+a= int(input("Seleccione un valor para a: "))
+b= int(input("Seleccione un valor para b: "))
+c= a+b
+print(c)
+print(a-b)
+print(c/2)
+print(a//b)
+print(a%b)
+print(a/b)
